ff668a864b1a74b3f2cdb26c5261d76ec7010f19
J6P6J6 <71605464+jooaumm787990r@users.noreply.github.com>
2023-01-17 13:38:19 -0300

    Update menuFunction.js

--- menuFunction.js
+++ menuFunction.js
@@ -1,9 +1,9 @@
-const AppBtn = document.getElementById('appMenuBtn');
-const NavBar = document.getElementById('app__navbar');
+const AppBtn = document.getElementById('menu_active');
+// const NavBar = document.getElementById('app__navbar');
 
 function MenuFunction(){
   AppBtn.id = "active";
-  NavBar.id = "navbar__active";
+//  NavBar.id = "navbar__active";
 }
 
 AppBtn.onclick = MenuFunction();
